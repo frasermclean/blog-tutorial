43a54aab733b132f16b57e657eb9a7b5ea2cd3ac
Fraser McLean <dev@frasermclean.com>
2025-03-10 15:57:21 +0800

    Add Cloudflare adapter and enable on-demand rendering for SSR page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import preact from '@astrojs/preact';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  adapter: cloudflare(),
 });
